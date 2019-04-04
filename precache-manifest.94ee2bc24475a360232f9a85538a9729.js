755bcd0275cda5ec3e2e0b41cbef38dd69e5eb1c
kshinji911 <kshinji911@gmail.com>
2019-04-04 13:02:54 -0400

    Updates

---
+++ precache-manifest.94ee2bc24475a360232f9a85538a9729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5fb262b2f4e8985515a0",
+    "url": "/clicky-game/static/js/main.8effadbc.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5fb262b2f4e8985515a0",
+    "url": "/clicky-game/static/css/main.112adb4c.chunk.css"
+  },
+  {
+    "revision": "a27b9faebde1d5946ba36ab9834a3bfd",
+    "url": "/clicky-game/index.html"
+  }
+];
